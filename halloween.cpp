42cf693012907d6ceb578f897c8dabcc580ca876
Marc <marc.casellas@est.fib.upc.edu>
2018-10-31 16:02:48 +0100

    Added noise to the chars

--- halloween.cpp
+++ halloween.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 
 using namespace std;
 
+vector<string> emojis{ "👻", "🔪", "🧛‍♂️", "💀", "☠️", "🎃"};
+
 string random_emoji() {
-	return "";
+	return emojis[rand() % emojis.size()];
 }
 
 int main() {
-	cout << random_emoji() << endl;
+    
+    cout << "👻 Enter the number of emojis:" << endl;
+    int num = 0;
+    cin >> num;
+    
+    srand(time(0));
+    
+    cout << "Beware of the spooky emojis! 🎃" << endl;
+    
+    for (int i = 0; i < num; i++){
+        int chars = 20;
+        int random = rand()%chars;
+        for (int j = 0; j < random; j++) cout << "._";
+        cout << random_emoji();
+        for (int j = 0; j < chars-random; j++) cout << "._";
+        cout << endl;
+    }
+    
 }
